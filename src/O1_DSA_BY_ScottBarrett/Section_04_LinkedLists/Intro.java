e7cf76ca66de8aeec53230deab66dc03a807dd26
Md Imtiaz Hossain <imtiaz005123@gmail.com>
2023-12-07 08:38:09 +0600

    Initial Commit.

--- src/O1_DSA_BY_ScottBarrett/Section_04_LinkedLists/Intro.java
+++ src/O1_DSA_BY_ScottBarrett/Section_04_LinkedLists/Intro.java
@@ -2,6 +2,6 @@ package O1_DSA_BY_ScottBarrett.Section_04_LinkedLists;
 
 public class Intro {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello ");
     }
 }
